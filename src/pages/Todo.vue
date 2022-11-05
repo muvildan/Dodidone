cb247df889c5053aa6db0e74a929e7997245aa08
Daniel <danielvilches98@gmail.com>
2022-11-05 15:10:34 +0100

    finito

--- src/pages/Todo.vue
+++ src/pages/Todo.vue
@@ -7,7 +7,7 @@ import LogOut from "../components/LogOut.vue";
   <Suspense>
     <template #default>
       <div
-        class="w-screen h-full xl:max-h-screen flex flex-col bg-gradient-to-r bg-clip-background background-transparent from-indigo-100 via-green-200 to-indigo-100 animate-background"
+        class="w-screen min-h-screen h-full xl:max-h-screen flex flex-col bg-gradient-to-r bg-clip-background background-transparent from-indigo-100 via-green-200 to-indigo-100 animate-background"
       >
         <TodoList />
         <LogOut />
