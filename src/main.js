b90562d5439ee231a27cdb7f6b9c368617fee889
Daniel <danielvilches98@gmail.com>
2022-11-05 03:16:34 +0100

    font

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import { createPinia } from "pinia";
 import piniaPersist from "pinia-plugin-persist";
 import App from "./App.vue";
 import router from "./router";
-import "/src/index.css";
+import "./index.css";
 
 const pinia = createPinia();
 pinia.use(piniaPersist);
